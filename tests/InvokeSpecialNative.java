4bd009676da5efc3fb2ea968937ecab2818c57bd
Max Bernstein <max@bernsteinbear.com>
2021-10-30 12:35:23 -0700

    Add test for invokespecial on native methods

---
+++ tests/InvokeSpecialNative.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+  static final String str = "Hello world!";
+  public static void main(String[] args) {
+    for (int i = 0; i < str.length(); i++) {
+      System.out.print(str.charAt(i));
+    }
+    System.out.println("");
+  }
+}
